09a61fe09ced696c2ec1d6bda97a6f41535f085c
Gustavo Ullmann <gustavoullmann@gmail.com>
2023-10-11 13:54:08 -0300

    Criação de teste de validacao de email e edição

--- cypress/e2e/exercicio-api.cy.js
+++ cypress/e2e/exercicio-api.cy.js
@@ -18,7 +18,7 @@ describe("Testes da Funcionalidade Usuários", () => {
     });
   });
 
-  it.only("Deve cadastrar um usuário com sucesso", () => {
+  it("Deve cadastrar um usuário com sucesso", () => {
     let fakerEmail = faker.internet.email();
     cy.cadastrarUsuario("Novo Usuário", fakerEmail, "testeSenha", "false").then(
       (response) => {
@@ -26,15 +26,43 @@ describe("Testes da Funcionalidade Usuários", () => {
         expect(response.body.message).to.equal(
           "Cadastro realizado com sucesso"
         );
-      });
+      }
+    );
   });
 
   it("Deve validar um usuário com email inválido", () => {
-    //TODO:
+    cy.cadastrarUsuario("Usuário Email Inválido", "emailinvalido.com", "testeSenha", "false").then(
+      (response) => {
+      expect(response.status).to.equal(400);
+      expect(response.body.email).to.equal("email deve ser um email válido")
+      }
+    )
   });
 
-  it("Deve editar um usuário previamente cadastrado", () => {
-    //TODO:
+  it.only("Deve editar um usuário previamente cadastrado", () => {
+    let fakerEmail = faker.internet.email();
+    let novoUsuarioId;
+    
+    cy.cadastrarUsuario("Editar Usuário", fakerEmail, "testeSenha", "true").then(
+      (response) => {
+        expect(response.status).to.equal(201);
+        novoUsuarioId = response.body._id;
+
+        cy.request({
+          method: "PUT",
+          url: `usuarios/${novoUsuarioId}`,
+          body: {
+            nome: "Nome Editado",
+            email: fakerEmail,
+            password: "senhaEditada",
+            administrador: "false",
+          }
+        }).then((response) => {
+          expect(response.status).to.equal(200);
+          expect(response.body.message).to.equal("Registro alterado com sucesso")
+        }) 
+      }
+    )
   });
 
   it("Deve deletar um usuário previamente cadastrado", () => {
